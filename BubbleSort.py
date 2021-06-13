5f0d50750c0b97e7ba3de8138bf01384ac47a151
Wesley Silva <wesleysilv@gmail.com>
2021-06-13 15:23:48 -0300

    verbose parameter adjustment

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,3 +1,13 @@
+#!/usr/bin/python3
+# encoding utf-8
+
+# BubbleSort Algorithm
+#   Step 1 - Step Starting with the first element(index = 0), compare the current element with the next element of the array.
+#   Step 2 - If the current element is greater than the next element of the array, swap them.
+#   Step 3 - If the current element is less than the next element, move to the next element. 
+#   Step 4 - Repeat Steps 1,2,and 3 until no exchange occurs.
+
+ 
 import random, time
 
 def BubbleSort(alist, verbose):
@@ -6,16 +16,16 @@ def BubbleSort(alist, verbose):
         task_completed = True
         for id,item in enumerate(alist):
             try:
-                if verbose == 'Y': print('{} > {} ?'.format(alist[id], alist[id+1]))
+                if verbose : print('{} > {} ?'.format(alist[id], alist[id+1]))
                 if alist[id] > alist[id+1]:
                     task_completed = False
-                    if verbose == 'Y': print(' - aux = {}'.format(item))
+                    if verbose : print(' - aux = {}'.format(item))
                     aux = item
-                    if verbose == 'Y': print(' - alist[id] = {}'.format(alist[id+1]))
+                    if verbose : print(' - alist[id] = {}'.format(alist[id+1]))
                     alist[id] = alist[id+1]
-                    if verbose == 'Y': print(' - alist[id+1] = {}'.format(aux))
+                    if verbose : print(' - alist[id+1] = {}'.format(aux))
                     alist[id+1] = aux
-                    if verbose == 'Y': print('{}'.format(alist))
+                    if verbose : print('{}'.format(alist))
             except:
                 pass
 
@@ -27,10 +37,11 @@ if __name__=='__main__':
         my_cluttered_list = list(range(int(list_length)))
         random.shuffle(my_cluttered_list)
         print(my_cluttered_list)
-        verbose = input('Do you want print the BubbleSort steps? [ y / n ] : ')
+        verbose_question= input('Do you want print the BubbleSort steps? [ y / n ] : ')
+        verbose = True if verbose_question.upper() == 'Y' else False
         print('Starting BubbleSort() ...')
         start = time.perf_counter()
-        my_ordered_list = BubbleSort(my_cluttered_list, verbose.upper())
+        my_ordered_list = BubbleSort(my_cluttered_list, verbose)
         print('\nMy final ordered list is {} '.format(my_ordered_list))
         print('Task executed in {} seconds.\n'.format(time.perf_counter()-start))
     except Exception as e:
